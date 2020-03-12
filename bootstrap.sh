9d905444d0dc8e1b3840d3a607f44ad74176f68c
Lucilio Correia de Oliveira <lo118422@grpoesp.com.br>
2020-03-12 19:32:05 -0300

    automate loading db dump and apache vhosts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
+# Setup
+PHP_VER="7.1";
+
 # Update Packages
 apt-get update
 # Upgrade Packages
@@ -17,18 +20,18 @@ apt-add-repository ppa:ondrej/php
 apt-get update
 
 # Install PHP
-apt-get install -y php7.2
+apt-get install -y php${PHP_VER}
 
 # PHP Apache Mod
-apt-get install -y libapache2-mod-php7.2
+apt-get install -y libapache2-mod-php${PHP_VER}
 
 # Restart Apache
 service apache2 restart
 
 # PHP Mods
-apt-get install -y php7.2-common
-apt-get install -y php7.2-mcrypt
-apt-get install -y php7.2-zip
+apt-get install -y php${PHP_VER}-common
+apt-get install -y php${PHP_VER}-zip
+apt-get install -y php${PHP_VER}-mcrypt
 
 # Set MySQL Pass
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
@@ -37,8 +40,29 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 # Install MySQL
 apt-get install -y mysql-server
 
+# Source SQL Files
+if [[ -d /vagrant/mysql ]]; then
+	for dumpfile in /vagrant/mysql/*.sql; do
+		mysql -u root -proot < $dumpfile
+	done
+fi
+
 # PHP-MYSQL lib
-apt-get install -y php7.2-mysql
+apt-get install -y php${PHP_VER}-mysql
+
+# Setup Apache
+a2enmod rewrite
+
+if [[ -d /vagrant/apache ]]; then
+	for file in /vagrant/apache/*-vhost.conf; do
+		cp -avf $file /etc/apache2/sites-available/$(basename ${file/-vhost/});
+		[ -f /etc/apache2/sites-enabled/$(basename ${file/-vhost/}) ] && rm -rf /etc/apache2/sites-enabled/$(basename ${file/-vhost/}); 
+		ln -sf /etc/apache2/sites-available/$(basename ${file/-vhost/}) /etc/apache2/sites-enabled/$(basename ${file/-vhost/});
+	done
+fi
 
 # Restart Apache
 sudo service apache2 restart
+
+# Done
+echo "*** READY TO GO ***\n\nreplace your /etc/hosts with the version included\n"
